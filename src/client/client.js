86384fafb0a66029b1784d6b77960d9072d2f621
Fares <fares.sioni@gmail.com>
2021-03-29 09:41:35 +0200

    Add base files

---
+++ src/client/client.js
@@ -0,0 +1,187 @@
+/* ******************************************************************
+ * Constantes de configuration
+ */
+const apiKey = "69617e9b-19db-4bf7-a33f-18d4e90ccab7";
+const serverUrl = "http://localhost:3000";
+
+/* ******************************************************************
+ * Gestion des tabs "Voter" et "Toutes les citations"
+ ******************************************************************** */
+
+/**
+ * Affiche/masque les divs "div-duel" et "div-tout"
+ * selon le tab indiqué dans l'état courant.
+ *
+ * @param {Etat} etatCourant l'état courant
+ */
+function majTab(etatCourant) {
+  console.log("CALL majTab");
+  const dDuel = document.getElementById("div-duel");
+  const dTout = document.getElementById("div-tout");
+  const tDuel = document.getElementById("tab-duel");
+  const tTout = document.getElementById("tab-tout");
+  if (etatCourant.tab === "duel") {
+    dDuel.style.display = "flex";
+    tDuel.classList.add("is-active");
+    dTout.style.display = "none";
+    tTout.classList.remove("is-active");
+  } else {
+    dTout.style.display = "flex";
+    tTout.classList.add("is-active");
+    dDuel.style.display = "none";
+    tDuel.classList.remove("is-active");
+  }
+}
+
+/**
+ * Mets au besoin à jour l'état courant lors d'un click sur un tab.
+ * En cas de mise à jour, déclenche une mise à jour de la page.
+ *
+ * @param {String} tab le nom du tab qui a été cliqué
+ * @param {Etat} etatCourant l'état courant
+ */
+function clickTab(tab, etatCourant) {
+  console.log(`CALL clickTab(${tab},...)`);
+  if (etatCourant.tab !== tab) {
+    etatCourant.tab = tab;
+    majPage(etatCourant);
+  }
+}
+
+/**
+ * Enregistre les fonctions à utiliser lorsque l'on clique
+ * sur un des tabs.
+ *
+ * @param {Etat} etatCourant l'état courant
+ */
+function registerTabClick(etatCourant) {
+  console.log("CALL registerTabClick");
+  document.getElementById("tab-duel").onclick = () =>
+    clickTab("duel", etatCourant);
+  document.getElementById("tab-tout").onclick = () =>
+    clickTab("tout", etatCourant);
+}
+
+/* ******************************************************************
+ * Gestion de la boîte de dialogue (a.k.a. modal) d'affichage de
+ * l'utilisateur.
+ * ****************************************************************** */
+
+/**
+ * Fait une requête GET authentifiée sur /whoami
+ * @returns une promesse du login utilisateur ou du message d'erreur
+ */
+function fetchWhoami() {
+  return fetch(serverUrl + "/whoami", { headers: { "x-api-key": apiKey } })
+    .then((response) => response.json())
+    .then((jsonData) => {
+      if (jsonData.status && Number(jsonData.status) != 200) {
+        return { err: jsonData.message };
+      }
+      return jsonData;
+    })
+    .catch((erreur) => ({ err: erreur }));
+}
+
+/**
+ * Fait une requête sur le serveur et insère le login dans
+ * la modale d'affichage de l'utilisateur.
+ * @returns Une promesse de mise à jour
+ */
+function lanceWhoamiEtInsereLogin() {
+  return fetchWhoami().then((data) => {
+    const elt = document.getElementById("elt-affichage-login");
+    const ok = data.err === undefined;
+    if (!ok) {
+      elt.innerHTML = `<span class="is-error">${data.err}</span>`;
+    } else {
+      elt.innerHTML = `Bonjour ${data.login}.`;
+    }
+    return ok;
+  });
+}
+
+/**
+ * Affiche ou masque la fenêtre modale de login en fonction de l'état courant.
+ *
+ * @param {Etat} etatCourant l'état courant
+ */
+function majModalLogin(etatCourant) {
+  const modalClasses = document.getElementById("mdl-login").classList;
+  if (etatCourant.loginModal) {
+    modalClasses.add("is-active");
+    lanceWhoamiEtInsereLogin();
+  } else {
+    modalClasses.remove("is-active");
+  }
+}
+
+/**
+ * Déclenche l'affichage de la boîte de dialogue du nom de l'utilisateur.
+ * @param {Etat} etatCourant
+ */
+function clickFermeModalLogin(etatCourant) {
+  etatCourant.loginModal = false;
+  majPage(etatCourant);
+}
+
+/**
+ * Déclenche la fermeture de la boîte de dialogue du nom de l'utilisateur.
+ * @param {Etat} etatCourant
+ */
+function clickOuvreModalLogin(etatCourant) {
+  etatCourant.loginModal = true;
+  majPage(etatCourant);
+}
+
+/**
+ * Enregistre les actions à effectuer lors d'un click sur les boutons
+ * d'ouverture/fermeture de la boîte de dialogue affichant l'utilisateur.
+ * @param {Etat} etatCourant
+ */
+function registerLoginModalClick(etatCourant) {
+  document.getElementById("btn-close-login-modal1").onclick = () =>
+    clickFermeModalLogin(etatCourant);
+  document.getElementById("btn-close-login-modal2").onclick = () =>
+    clickFermeModalLogin(etatCourant);
+  document.getElementById("btn-open-login-modal").onclick = () =>
+    clickOuvreModalLogin(etatCourant);
+}
+
+/* ******************************************************************
+ * Initialisation de la page et fonction de mise à jour
+ * globale de la page.
+ * ****************************************************************** */
+
+/**
+ * Mets à jour la page (contenu et événements) en fonction d'un nouvel état.
+ *
+ * @param {Etat} etatCourant l'état courant
+ */
+function majPage(etatCourant) {
+  console.log("CALL majPage");
+  majTab(etatCourant);
+  majModalLogin(etatCourant);
+  registerTabClick(etatCourant);
+  registerLoginModalClick(etatCourant);
+}
+
+/**
+ * Appelé après le chargement de la page.
+ * Met en place la mécanique de gestion des événements
+ * en lançant la mise à jour de la page à partir d'un état initial.
+ */
+function initClientCitations() {
+  console.log("CALL initClientCitations");
+  const etatInitial = {
+    tab: "duel",
+    loginModal: false,
+  };
+  majPage(etatInitial);
+}
+
+// Appel de la fonction init_client_duels au après chargement de la page
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Exécution du code après chargement de la page");
+  initClientCitations();
+});
